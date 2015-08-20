521579f1a5f6f812c35a8a535f319ae2d62a12e0
Michael Lynch <mtlynch@google.com>
2015-08-19 22:07:40 -0400

    Splitting out ApiClient from Client

--- dataserv_client/api.py
+++ dataserv_client/api.py
@@ -21,91 +21,121 @@ _timedelta = datetime.timedelta
 _now = datetime.datetime.now
 
 
-class Client(object):
+class ApiClient(object):
 
-    def __init__(self, address=None, url=common.DEFAULT_URL, debug=False,
-                 max_size=common.DEFAULT_MAX_SIZE,
-                 store_path=common.DEFAULT_STORE_PATH,
-                 connection_retry_limit=common.DEFAULT_CONNECTION_RETRY_LIMIT,
-                 connection_retry_delay=common.DEFAULT_CONNECTION_RETRY_DELAY):
+    def __init__(self, server_url, client_address, connection_retry_limit,
+                 connection_retry_delay):
+        self._server_url = server_url
 
-        self.url = url
-        self.debug = debug
-        self.address = address
-        self.max_size = deserialize.byte_count(max_size)
-        self.store_path = os.path.realpath(store_path)
-
-        if int(connection_retry_limit) < 0:
-            raise exceptions.InvalidArgument()
-        self.connection_retry_limit = int(connection_retry_limit)
-
-        if int(connection_retry_delay) < 0:
-            raise exceptions.InvalidArgument()
-        self.connection_retry_delay = int(connection_retry_delay)
-
-        # ensure storage dir exists
-        if not os.path.exists(self.store_path):
-            os.makedirs(self.store_path)
-
-    def _ensure_address_given(self):
-        if not self.address:  # TODO ensure address is valid
+        if not client_address:
             raise exceptions.AddressRequired()
+        self._client_address = client_address
 
-    def version(self):
-        print(__version__)
-        return __version__
+        if connection_retry_limit < 0:
+            raise exceptions.InvalidArgument()
+        self._connection_retry_limit = connection_retry_limit
 
-    def _url_query(self, api_call, retries=0):
+        if connection_retry_delay < 0:
+            raise exceptions.InvalidArgument()
+        self._connection_retry_delay = connection_retry_delay
+
+    def _url_query(self, api_path, retries=0):
         try:
-            if self.debug:
-                print("Query url: " + self.url + api_call)
-            response = urllib.request.urlopen(self.url + api_call)
+            response = urllib.request.urlopen(self._server_url + api_path)
             if response.code == 200:
                 return True
             return False  # pragma: no cover
 
         except urllib.error.HTTPError as e:
             if e.code == 409:
-                raise exceptions.AddressAlreadyRegistered(self.address,
-                                                          self.url)
+                raise exceptions.AddressAlreadyRegistered(self._client_address,
+                                                          self._server_url)
             elif e.code == 404:
-                raise exceptions.FarmerNotFound(self.url)
+                raise exceptions.FarmerNotFound(self._server_url)
             elif e.code == 400:
-                raise exceptions.InvalidAddress(self.address)
+                raise exceptions.InvalidAddress(self._client_address)
             elif e.code == 500:  # pragma: no cover
-                raise exceptions.FarmerError(self.url)  # pragma: no cover
+                raise exceptions.FarmerError(self._server_url)
             else:
                 raise e  # pragma: no cover
         except HTTPException:
-            self._handle_connection_error(api_call, retries)
+            self._handle_connection_error(api_path, retries)
         except urllib.error.URLError:
-            self._handle_connection_error(api_call, retries)
+            self._handle_connection_error(api_path, retries)
 
-    def _handle_connection_error(self, api_call, retries):
-        if retries >= self.connection_retry_limit:
-            raise exceptions.ConnectionError(self.url)
-        time.sleep(self.connection_retry_delay)
-        return self._url_query(api_call, retries + 1)
+    def _handle_connection_error(self, api_path, retries):
+        if retries >= self._connection_retry_limit:
+            raise exceptions.ConnectionError(self._server_url)
+        time.sleep(self._connection_retry_delay)
+        return self._url_query(api_path, retries + 1)
+
+    def server_url(self):
+        return self._server_url
+
+    def client_address(self):
+        return self._client_address
+
+    def register(self):
+        """Attempt to register this client address."""
+        return self._url_query("/api/register/%s" % self._client_address)
+
+    def ping(self):
+        """Send a heartbeat message for this client address."""
+        return self._url_query("/api/ping/%s" % self._client_address)
+
+    def height(self, height):
+        """Set the height claim for this client address."""
+        return self._url_query('/api/height/%s/%s' % (self._client_address,
+                                                      height))
+
+
+class Client(object):
+
+    def __init__(self, client_address=None, url=common.DEFAULT_URL, debug=False,
+                 max_size=common.DEFAULT_MAX_SIZE,
+                 store_path=common.DEFAULT_STORE_PATH,
+                 connection_retry_limit=common.DEFAULT_CONNECTION_RETRY_LIMIT,
+                 connection_retry_delay=common.DEFAULT_CONNECTION_RETRY_DELAY):
+
+        self._validate_client_address(client_address)
+        self._api_client = ApiClient(url, client_address,
+                                     connection_retry_limit,
+                                     connection_retry_delay)
+        self.debug = debug
+        self.max_size = deserialize.byte_count(max_size)
+        self.store_path = os.path.realpath(store_path)
+
+        # ensure storage dir exists
+        if not os.path.exists(self.store_path):
+            os.makedirs(self.store_path)
+
+    def _validate_client_address(self, client_address):
+        if not client_address:  # TODO ensure address is valid
+            raise exceptions.AddressRequired()
+
+    def version(self):
+        print(__version__)
+        return __version__
 
     def register(self):
         """Attempt to register the config address."""
-        self._ensure_address_given()
-        registered = self._url_query("/api/register/{0}".format(self.address))
-        if registered:
-            print("Address {0} now registered on {1}.".format(self.address,
-                                                              self.url))
-        return registered
+        if self._api_client.register():
+            print("Address %s now registered on %s." % (
+                self._api_client.client_address(),
+                self._api_client.server_url()))
+            return True
+        else:
+            return False
 
     def ping(self):
         """Attempt keep-alive with the server."""
-        self._ensure_address_given()
-        print("Pinging {0} with address {1}.".format(self.url, self.address))
-        return self._url_query("/api/ping/{0}".format(self.address))
+        print("Pinging %s with address %s." % (
+            self._api_client.server_url(), self._api_client.client_address()))
+        return self._api_client.ping()
 
     def poll(self, register_address=False, delay=common.DEFAULT_DELAY,
              limit=None):
         """TODO doc string"""
-        self._ensure_address_given()
         stop_time = _now() + _timedelta(seconds=int(limit)) if limit else None
 
         if register_address:
@@ -120,14 +150,13 @@ class Client(object):
 
     def build(self, cleanup=False, rebuild=False):
         """TODO doc string"""
-        self._ensure_address_given()
 
-        def on_generate_shard(height, seed, file_hash):
-            self._url_query('/api/height/{0}/{1}'.format(self.address, height))
+        def on_generate_shard(height, unused_seed, unused_file_hash):
+            self._api_client.height(height)
         bldr = builder.Builder(self.address, common.SHARD_SIZE, self.max_size,
                                on_generate_shard=on_generate_shard)
         generated = bldr.build(self.store_path, debug=self.debug,
                                cleanup=cleanup, rebuild=rebuild)
         height = len(generated)
-        self._url_query('/api/height/{0}/{1}'.format(self.address, height))
+        self._api_client.height(height)
         return generated
--- tests/test_client_unit.py
+++ tests/test_client_unit.py
@@ -25,7 +25,7 @@ class MockURLError(urllib.error.URLError):
         pass
 
 
-class TestClientBase(unittest.TestCase):
+class TestApiClientBase(unittest.TestCase):
 
     def setUp(self):
         urlopen_patch = mock.patch.object(urllib.request, "urlopen",
@@ -33,10 +33,12 @@ class TestClientBase(unittest.TestCase):
         self.addCleanup(urlopen_patch.stop)
         urlopen_patch.start()
 
-        self.client = api.Client(_MOCK_ADDRESS, url=_MOCK_SERVER_URL)
+        self.client = api.ApiClient(_MOCK_SERVER_URL, _MOCK_ADDRESS,
+                                    connection_retry_limit=0,
+                                    connection_retry_delay=0)
 
 
-class TestClientRegister(TestClientBase):
+class TestApiClientRegister(TestApiClientBase):
 
     def test_register(self):
         urllib.request.urlopen.return_value.code = 200
@@ -58,20 +60,13 @@ class TestClientRegister(TestClientBase):
         urllib.request.urlopen.assert_called_once_with(
             _MOCK_SERVER_URL + "/api/register/" + _MOCK_ADDRESS)
 
-    def test_invalid_farmer(self):
-        urllib.request.urlopen.side_effect = MockHTTPError(404)
-        with self.assertRaises(exceptions.FarmerNotFound):
-            self.client.register()
-        urllib.request.urlopen.assert_called_once_with(
-            _MOCK_SERVER_URL + "/api/register/" + _MOCK_ADDRESS)
-
     def test_address_required(self):
         with self.assertRaises(exceptions.AddressRequired):
             api.Client().register()
         self.assertFalse(urllib.request.urlopen.called)
 
 
-class TestClientPing(TestClientBase):
+class TestApiClientPing(TestApiClientBase):
 
     def test_ping(self):
         urllib.request.urlopen.return_value.code = 200
@@ -99,15 +94,43 @@ class TestClientPing(TestClientBase):
         self.assertFalse(urllib.request.urlopen.called)
 
 
-class TestConnectionRetry(TestClientBase):
+class TestApiClientHeight(TestApiClientBase):
+
+    def test_height(self):
+        urllib.request.urlopen.return_value.code = 200
+        height = 42
+        self.assertTrue(self.client.height(42))
+        urllib.request.urlopen.assert_called_once_with(
+            '%s/api/height/%s/%s' % (_MOCK_SERVER_URL, _MOCK_ADDRESS, height))
+
+    def test_invalid_address(self):
+        urllib.request.urlopen.side_effect = MockHTTPError(400)
+        height = 42
+        with self.assertRaises(exceptions.InvalidAddress):
+            self.client.height(height)
+        urllib.request.urlopen.assert_called_once_with(
+            '%s/api/height/%s/%s' % (_MOCK_SERVER_URL, _MOCK_ADDRESS, height))
+
+    def test_invalid_farmer(self):
+        urllib.request.urlopen.side_effect = MockHTTPError(404)
+        height = 42
+        with self.assertRaises(exceptions.FarmerNotFound):
+            self.client.height(height)
+        urllib.request.urlopen.assert_called_once_with(
+            '%s/api/height/%s/%s' % (_MOCK_SERVER_URL, _MOCK_ADDRESS, height))
+
+
+class TestApiClientConnectionRetry(TestApiClientBase):
 
     def test_invalid_retry_limit(self):
         with self.assertRaises(exceptions.InvalidArgument):
-            api.Client(connection_retry_limit=-1)
+            api.ApiClient(_MOCK_SERVER_URL, _MOCK_ADDRESS,
+                          connection_retry_limit=-1)
 
     def test_invalid_retry_delay(self):
         with self.assertRaises(exceptions.InvalidArgument):
-            api.Client(connection_retry_delay=-1)
+            api.ApiClient(_MOCK_SERVER_URL, _MOCK_ADDRESS,
+                          connection_retry_delay=-1)
 
     @mock.patch("time.sleep")
     def test_no_retry(self, mock_sleep):
@@ -131,13 +154,6 @@ class TestConnectionRetry(TestClientBase):
                          "Expect 1 call + 5 retries = 6 calls")
         mock_sleep.assert_has_calls([mock.call(5), mock.call(5), mock.call(5),
                                      mock.call(5), mock.call(5)])
-
-
-class TestClientBuild(TestClientBase):
-
-    def test_address_required(self):
-        with self.assertRaises(exceptions.AddressRequired):
-            api.Client().build()
 
 
 if __name__ == "__main__":
